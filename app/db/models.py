943ee5a2484e00ed785f4f89becf737f3fad607f
Pavlo Semenikhin <pashock1337@gmail.com>
2025-10-28 18:15:47 +0200

    Add Event model

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,27 @@
-from sqlalchemy.orm import DeclarativeBase
+import uuid
+from datetime import datetime, timezone
+from sqlalchemy import Integer, DateTime, func, String, JSON
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Event(Base):
+    __tablename__ = "events"
+    __table_args__ = {"extend_existing": True}
+
+    event_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
+    occurred_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=lambda: datetime.now(timezone.utc),
+        server_default=func.now()
+    )
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    event_type: Mapped[str] = mapped_column(String, nullable=False)
+    properties: Mapped[dict] = mapped_column(JSON, nullable=False, default=dict)
