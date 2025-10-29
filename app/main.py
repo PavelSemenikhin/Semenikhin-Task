65fff8899093617400af9a3dce704cf568281573
Pavlo Semenikhin <pashock1337@gmail.com>
2025-10-29 17:57:11 +0200

    Include events router

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.api.routes.stats import router as stats_router
+from app.api.routes.events import router as events_router
 app = FastAPI()
 
 app.include_router(stats_router)
+app.include_router(events_router)
 @app.get("/")
 async def root():
     return {"message": "done"}
