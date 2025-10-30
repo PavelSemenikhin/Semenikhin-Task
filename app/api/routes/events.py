106e727d253a4ee925c26ffcfb3ee830e9530afd
Pavlo Semenikhin <pashock1337@gmail.com>
2025-10-30 02:35:54 +0200

    Add rate limiter

--- app/api/routes/events.py
+++ app/api/routes/events.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 
 from app.api.schemas.events import EventBatch
 from app.core.nats_client import publish
+from app.core.rate_limiter import rate_limiter
 
 router = APIRouter(prefix="/events", tags=["Events"])
 
@@ -10,8 +11,11 @@ router = APIRouter(prefix="/events", tags=["Events"])
     "/ingest/",
     status_code=status.HTTP_202_ACCEPTED,
     description="Publish events to NATS for async ingestion",
+    dependencies=[Depends(rate_limiter)]
 )
-async def events_ingestion(payload: EventBatch):
+async def events_ingestion(
+        payload: EventBatch,
+):
     published = 0
 
     for event in payload.events:
---
+++ app/core/rate_limiter.py
@@ -0,0 +1,23 @@
+import time
+from fastapi import HTTPException, status, Request
+
+RATE_LIMIT = 10
+WINDOW = 10
+requests_log = {}
+
+
+async def rate_limiter(request: Request):
+    client_ip = request.client.host
+    now = time.time()
+
+    timestamps = requests_log.get(client_ip, [])
+    timestamps = [t for t in timestamps if now - t < WINDOW]
+
+    if len(timestamps) >= RATE_LIMIT:
+        raise HTTPException(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+            detail=f"Rate limit exceeded. Try again after {WINDOW} seconds."
+        )
+
+    timestamps.append(now)
+    requests_log[client_ip] = timestamps
